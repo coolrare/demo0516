f8cb0afe4a52fd611bb7b913975fadb074120749
Will保哥 <doggy.huang@gmail.com>
2017-05-16 14:12:33 +0800

    建立 Article 元件，並將 item 資料傳入

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @Input()
+  item: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
